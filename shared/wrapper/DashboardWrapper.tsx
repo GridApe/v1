cb51599832a840bea4f6f555bee6f80e6631bf7b
Collins Ikechukwu <collinschristroa@gmail.com>
2024-10-20 17:49:26 +0100

    feat(ui): add Input, Table, and Tabs components
    feat(shared): add Rocket component
    
    feat(public): add rocket.svg asset
    
    fix(wrapper): adjust DashboardWrapper container height

--- shared/wrapper/DashboardWrapper.tsx
+++ shared/wrapper/DashboardWrapper.tsx
@@ -49,7 +49,7 @@ export default function DashboardWrapper({
           }}
           transition={{ type: 'spring', stiffness: 300, damping: 30 }}
         >
-          <div className="container mx-auto p-6 bg-[#fffae9de] rounded-s-3xl">
+          <div className="container mx-auto min-h-svh p-6 bg-[#fffae9de] rounded-s-3xl">
             <Button
               variant="outline"
               size="icon"
