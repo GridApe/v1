e587ad0af9ed3d50af432f2e735af265f147eedb
Collins Ikechukwu <collinschristroa@gmail.com>
2024-12-02 17:17:35 +0100

    feat(user settings): add route for domain addition in user settings

---
+++ app/api/user/settings/add-domain/route.ts
@@ -0,0 +1,7 @@
+import { handleApiRequest } from '@/lib/api-utils';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  return handleApiRequest(request, '/user/domain-verification', 'POST');
+}
+
