e0fd7660390a68b231a4f5ddc09d638fa77bdb93
Collins Ikechukwu <collinschristroa@gmail.com>
2024-12-06 11:08:16 +0100

    fix(user): correct endpoint in user templates route

--- app/api/user/templates/all/route.ts
+++ app/api/user/templates/all/route.ts
@@ -2,5 +2,5 @@ import { handleApiRequest } from '@/lib/api-utils';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
-  return handleApiRequest(request, '/user/tem', 'GET');
+  return handleApiRequest(request, '/user/email-templates', 'GET');
 }
