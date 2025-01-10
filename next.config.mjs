1837afb1dcb52faaaaca2887802016c3e03aec17
Collins Ikechukwu <collinschristroa@gmail.com>
2025-01-10 09:56:40 +0100

    feat(next.config): add standalone output and experimental optimizations

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+  reactStrictMode: false,
+  output: 'standalone',
+  experimental: {
+    optimizeCss: true,
+    turbotrace: {
+      memoryLimit: 4096,
+    },
+  },
+  webpack: (config, { isServer }) => {
+    config.cache = false 
+    
+    return config
+  }
 };
 
 export default nextConfig;
