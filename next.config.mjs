16ceef26d08d6013394139d9652a05ebcb6239b4
Collins Ikechukwu <collinschristroa@gmail.com>
2025-01-10 09:59:25 +0100

    refactor: remove experimental configuration options from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  output: 'standalone',
-  experimental: {
-    optimizeCss: true,
-    turbotrace: {
-      memoryLimit: 4096,
-    },
-  },
   webpack: (config, { isServer }) => {
     config.cache = false 
     
